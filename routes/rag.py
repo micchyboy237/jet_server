917a574a69c59cafcc0de111a48106362287c0e1
Jethro Estrada <jethroestrada237@gmail.com>
2025-02-10 00:30:48 +0800

    Updates

--- routes/rag.py
+++ routes/rag.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import time
 from typing import Any, Awaitable, Generator, Literal, Optional
 from deeplake.core.vectorstore.deeplake_vectorstore import VectorStore
 from jet.llm.ollama.constants import OLLAMA_LARGE_EMBED_MODEL
@@ -11,7 +12,7 @@ from jet.llm.main.prompts_generator import PromptsGenerator
 from jet.llm.ollama.base import Ollama
 from jet.transformers.formatters import format_json
 from pydantic import BaseModel
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Request
 from fastapi.responses import StreamingResponse
 from jet.vectors import get_source_node_attributes
 from jet.logger import logger
@@ -23,36 +24,54 @@ router = APIRouter()
 # Create default RAG instance (will be updated in the endpoint)
 rag_global_dict: dict[str, object] = {}
 
+rag_dir: str = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/data/jet-resume/data"
+extensions: list[str] = [".md", ".mdx", ".rst"]
+system: str = (
+    "You are a job applicant providing tailored responses during an interview.\n"
+    "Always answer questions using the provided context as if it is your resume, "
+    "and avoid referencing the context directly.\n"
+    "Some rules to follow:\n"
+    "1. Never directly mention the context or say 'According to my resume' or similar phrases.\n"
+    "2. Provide responses as if you are the individual described in the context, focusing on professionalism and relevance."
+)
+chunk_size: int = 1024
+chunk_overlap: int = 40
+sub_chunk_sizes: list[int] = [512, 256, 128]
+with_hierarchy: bool = True
+top_k: Optional[int] = None
+model: str = "llama3.2"
+embed_model: str = OLLAMA_LARGE_EMBED_MODEL
+mode: Literal["fusion", "hierarchy",
+              "deeplake", "faiss", "graph_nx"] = "fusion"
+store_path: str = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_server/.cache/deeplake/store_1"
+score_threshold: float = 0.0
+split_mode: list[Literal["markdown", "hierarchy"]] = []
+
+contexts: list[str] = []
+
 
 # Define the schema for input queries
 class QueryRequest(BaseModel):
     query: str
-    rag_dir: str = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/data/jet-resume/data"
-    extensions: list[str] = [".md", ".mdx", ".rst"]
-    system: str = (
-        "You are a job applicant providing tailored responses during an interview.\n"
-        "Always answer questions using the provided context as if it is your resume, "
-        "and avoid referencing the context directly.\n"
-        "Some rules to follow:\n"
-        "1. Never directly mention the context or say 'According to my resume' or similar phrases.\n"
-        "2. Provide responses as if you are the individual described in the context, focusing on professionalism and relevance."
-    )
-    chunk_size: int = 1024
-    chunk_overlap: int = 40
-    sub_chunk_sizes: list[int] = [512, 256, 128]
-    with_hierarchy: bool = True
-    top_k: Optional[int] = None
-    model: str = "llama3.2"
-    embed_model: str = OLLAMA_LARGE_EMBED_MODEL
+    rag_dir: str = rag_dir
+    extensions: list[str] = extensions
+    system: str = system
+    chunk_size: int = chunk_size
+    chunk_overlap: int = chunk_overlap
+    sub_chunk_sizes: list[int] = sub_chunk_sizes
+    with_hierarchy: bool = with_hierarchy
+    top_k: Optional[int] = top_k
+    model: str = model
+    embed_model: str = embed_model
     mode: Literal["fusion", "hierarchy",
-                  "deeplake", "faiss", "graph_nx"] = "fusion"
-    store_path: str = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_server/.cache/deeplake/store_1"
-    score_threshold: float = 0.0
-    split_mode: list[Literal["markdown", "hierarchy"]] = []
+                  "deeplake", "faiss", "graph_nx"] = mode
+    store_path: str = store_path
+    score_threshold: float = score_threshold
+    split_mode: list[Literal["markdown", "hierarchy"]] = split_mode
 
 
 class SearchRequest(QueryRequest):
-    contexts: list[str] = []
+    contexts: list[str] = contexts
 
 
 class Metadata(BaseModel):
@@ -241,6 +260,16 @@ async def get_nodes(query_request: QueryRequest):
     return data
 
 
+@router.post("/stream-nodes", response_model=NodesResponse)
+async def stream_nodes(query_request: QueryRequest):
+    headers = {
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Content-Type": "text/event-stream",
+    }
+    return StreamingResponse(event_stream_nodes(query_request), headers=headers)
+
+
 def event_stream_nodes(query_request: QueryRequest) -> Generator[str, None, None]:
     """Generator function to yield events for streaming."""
     query_request_dict = query_request.__dict__.copy()
@@ -264,14 +293,32 @@ def event_stream_nodes(query_request: QueryRequest) -> Generator[str, None, None
         logger.success(format_json(transformed_nodes))
 
 
-@router.post("/stream-nodes", response_model=NodesResponse)
-async def stream_nodes(query_request: QueryRequest):
+@router.post("/sample-stream")
+async def sample_stream(request: Request):
+    """
+    Endpoint to stream responses for a given thread_id.
+    """
+
+    body = await request.body()
+    request_params_str = body.decode('utf-8')
+    request_params = json.loads(request_params_str)
+
+    logger.debug("sample_stream:")
+    logger.orange(format_json(request_params))
+
     headers = {
         "Cache-Control": "no-cache",
         "Connection": "keep-alive",
         "Content-Type": "text/event-stream",
     }
-    return StreamingResponse(event_stream_nodes(query_request), headers=headers)
+    return StreamingResponse(event_sample_stream(request_params), headers=headers)
+
+
+def event_sample_stream(request_params):
+    """Generator function to yield events for streaming."""
+    for i in range(10):  # Example: Stream 10 chunks
+        yield f"data: Message {i}\n\n"
+        time.sleep(1)  # Simulate delay between chunks
 
 
 if __name__ == "__main__":

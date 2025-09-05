3b63335438985aeea09e114979c32108dbfe3cc8
Jethro Estrada <jethroestrada237@gmail.com>
2025-09-05 10:37:35 +0800

    Updates

--- start_mlx_server.sh
+++ start_mlx_server.sh
@@ -1,1 +1,1 @@
-mlx_lm.server --host 0.0.0.0 --port 8080 --chat-template-args '{"enable_thinking":false}' --model mlx-community/Mistral-Nemo-Instruct-2407-4bit
+mlx_lm.server --host 0.0.0.0 --port 8080 --use-default-chat-template --model mlx-community/Mistral-7B-Instruct-v0.3-4bit
